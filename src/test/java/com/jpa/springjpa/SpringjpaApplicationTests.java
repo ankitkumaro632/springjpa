e5da1a29c827f1fc17110c2c74557ea1f28661bc
Ankit Kumar <105793993+ankitkumaro632@users.noreply.github.com>
2023-03-07 10:38:24 +0530

    applying sonarqube plugins

--- src/test/java/com/jpa/springjpa/SpringjpaApplicationTests.java
+++ src/test/java/com/jpa/springjpa/SpringjpaApplicationTests.java
@@ -1,13 +1,26 @@
 package com.jpa.springjpa;
 
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringjpaApplicationTests {
+class SpringjpaApplicationTests extends TestCase {
 
+	public SpringjpaApplicationTests(String testName){
+		setName(testName);
+	}
+
+	public static Test suite() {
+		return (Test) new TestSuite(SpringjpaApplicationTests.class);
+	}
 	@Test
 	void contextLoads() {
+		App a = new App();
+		a.m1();
+		a.getData();
+		assertTrue(true);
 	}
 
 }
